fc192544bac44d22f5d86471941b1c716fc9403b
Rafael Barros <rafaelbarros.df@gmail.com>
2021-04-27 23:28:25 -0300

    :tada: Initial commit

---
+++ src/test/java/br/com/mp/quarkusmovie/resources/NativeMovieResourceIT.java
@@ -0,0 +1,9 @@
+package br.com.mp.quarkusmovie.resources;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeMovieResourceIT extends MovieResourceTest {
+
+    // Execute the same tests but in native mode.
+}
